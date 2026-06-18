b4c980a8592122551705e9f2b70b7e2b05df2097
mavillanueva24 <mario.villanueva@ucsp.edu.pe>
2026-06-17 23:41:47 -0500

    Uso de AbstractHttpConfigurer para configuraciones de CSRF y login evitando redundancia. Fix #26

--- src/main/java/com/springspartans/shopkart/config/SecurityConfig.java
+++ src/main/java/com/springspartans/shopkart/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
@@ -19,12 +20,10 @@ public class SecurityConfig {
             .authorizeHttpRequests(authorize -> authorize
                 .anyRequest().permitAll()			// Allow all requests without authentication
             )
-            .csrf(csrf -> csrf.disable())			// Optionally disable CSRF protection
-            .formLogin(form -> form
-                .disable() 							// Disable default form login
-            )
-            .logout(logout -> logout.disable())		// Disable default logout
-            .httpBasic(basic -> basic.disable());	// Disable HTTP Basic authentication									
+            .csrf(AbstractHttpConfigurer::disable)			// Optionally disable CSRF protection
+            .formLogin(AbstractHttpConfigurer::disable) 							// Disable default form login
+            .logout(AbstractHttpConfigurer::disable)		// Disable default logout
+            .httpBasic(AbstractHttpConfigurer::disable);	// Disable HTTP Basic authentication									
     	
     	return http.build();
     }
